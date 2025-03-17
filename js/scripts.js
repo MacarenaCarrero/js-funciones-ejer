f3ec14a665af44cc37d07bb7f54764f8e4517d74
Marialafu <macarena.c.rios@gmail.com>
2025-03-17 11:59:22 +0100

    js-funciones-ejer

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//primero
+function camilaJob(calls, hours) {
+  const result = calls / hours;
+  console.log("El total de llamadas que atiende es " + result);
+}
+
+camilaJob(120, 4);
+
+//segundo
+
+function begoPencil(box, pencils) {
+  const result = box * pencils;
+  console.log("El total de lápices es " + result);
+}
+begoPencil(5, 12);
