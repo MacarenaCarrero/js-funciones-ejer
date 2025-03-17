f983889954551ffc23ee9449d0d3fe3150e1db94
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-03-17 18:48:25 +0100

    js-funciones-ejer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//primero
+//uno
 function camilaJob(calls, hours) {
   const result = calls / hours;
   console.log("El total de llamadas que atiende es " + result);
@@ -6,7 +6,7 @@ function camilaJob(calls, hours) {
 
 camilaJob(120, 4);
 
-//segundo
+//dos
 
 function begoPencil(box, pencils) {
   const result = box * pencils;
@@ -14,6 +14,231 @@ function begoPencil(box, pencils) {
 }
 begoPencil(5, 12);
 
-//tercero
+//tres
+function camilaCandy(candy, divider) {
+  const result = candy % divider;
+  console.log("A Camila le sobrarán " + result + " después del reparto.");
+}
 
-function  runSabrina()
+camilaCandy(25, 4);
+
+//cuatro
+
+function runSabrina(horas) {
+  const kmHora = 90 / 3;
+  let result = kmHora * horas;
+  console.log("Sabrina recorrerá " + result + "kilómetros en dos horas");
+}
+runSabrina(2);
+
+//cinco
+
+function appleMacarena(manzanas, cajas) {
+  const boxApple = manzanas / cajas;
+  let result = boxApple;
+  console.log("Cada caja tendrá " + result + " manzanas.");
+}
+appleMacarena(240, 8);
+
+//seis
+
+function screwsBego(screws, littleBox) {
+  const boxScrews = screws % littleBox;
+  let result = boxScrews;
+  console.log("Quedan fuera " + result + " tornillos.");
+}
+screwsBego(57, 10);
+//siete
+
+function moneyAbby(euros, cuantity) {
+  const cuantityMission = euros * cuantity;
+  let result = cuantityMission;
+  console.log("Cobrará un toral de " + result + " euros al día.");
+}
+moneyAbby(35, 5);
+//ocho
+
+function cookiesSabrina(cuantity, bags) {
+  const cookiesRest = cuantity % bags;
+  let result = cookiesRest;
+  console.log("Sobrarán " + result + " galletas");
+}
+cookiesSabrina(92, 8);
+//nueve
+
+function booksCamila(books, newBooks) {
+  const addBooks = books + newBooks;
+  let result = addBooks;
+  console.log("Camila ahora tiene " + result + " libros.");
+}
+booksCamila(45, 23);
+//diez
+
+function oldCoins(coins, showcase) {
+  const coinRest = coins % showcase;
+  let result = coinRest;
+  console.log("No cabrán en la vitrina " + result + " monedas.");
+}
+oldCoins(123, 5);
+//once
+
+function calculateKm(liters, ability) {
+  const kilometers = (ability * 100) / liters;
+  let result = kilometers;
+  console.log("Bego recorrerá con 42 litros, " + result + " kilómetros.");
+}
+calculateKm(7, 42);
+
+//doce
+function calculateChickens(chickenCoop, chicken) {
+  const calculateAll = chickenCoop * chicken;
+  let result = calculateAll;
+  console.log("En total hay " + result + " gallinas.");
+}
+calculateChickens(6, 25);
+//trece
+
+//catorce
+
+function begoRun(km, kilometers) {
+  const addKm = km + kilometers;
+  let result = addKm;
+  console.log("Bego corrió en total " + result + " kilómetros.");
+}
+begoRun(3, 4);
+//quince
+
+function cuantityPapers(papers, group) {
+  const groupPapers = papers % group;
+  let result = groupPapers;
+  console.log("Quedan fuera " + result + " hojas.");
+}
+cuantityPapers(100, 6);
+//dieciseis
+
+function distributeMoney(money, accounts) {
+  const divideMoney = money / accounts;
+  let result = divideMoney;
+  console.log("Cada cuenta tendrá " + result + " euros. De nada.");
+}
+distributeMoney(30000, 4);
+
+//diecisiete
+
+function zombies(killed) {
+  const cuantityKilled = killed / 7;
+  let result = cuantityKilled;
+  console.log("Al día mató " + result + " zombies.");
+}
+zombies(5201);
+
+//dieciocho
+
+function marblesCuantity(package, marbles) {
+  const addMarbles = package * marbles;
+  let result = addMarbles;
+  console.log("Bego tiene en total " + result + " canicas.");
+}
+marblesCuantity(3, 24);
+
+//diecinueve
+
+function coinsBrothers(coins, inputCamila) {
+  const totallyCoins = coins - inputCamila;
+  let result = totallyCoins;
+  console.log("El hermano de Camila ha aportado " + result + " monedas.");
+}
+coinsBrothers(135, 78);
+
+//veinte
+
+function legoPiece(pieces, lot) {
+  const totalPieces = pieces % lot;
+  let result = totalPieces;
+  console.log("Quedarán " + result + " piezas sueltas.");
+}
+legoPiece(500, 80);
+
+//veintiuno
+
+function distribute(cookies, dogs) {
+  const distributeCookies = cookies / dogs;
+  let result = distributeCookies;
+  console.log("Cada perro tendrá " + result + " galletas.");
+}
+distribute(72, 9);
+
+//veintidos
+
+function knives(cuantity, price) {
+  const pricesTotally = cuantity * price;
+  let result = pricesTotally;
+  console.log("Tendrá que pagar un total de " + result + " euros.");
+}
+knives(5, 18);
+
+//veintitres
+
+function bagsCookies(cookies, bags) {
+  const addCookies = cookies / bags;
+  let result = addCookies;
+  console.log("Llenará un total de " + result + " bolsas.");
+}
+bagsCookies(480, 40);
+
+//veinticuatro
+function kmDay(km, days) {
+  const kilometersXday = km / days;
+  let result = kilometersXday;
+  console.log("Recorrerá " + result + " kilómetros por día.");
+}
+kmDay(1200, 4);
+
+//veinticinco
+function saving(month1, month2) {
+  const totallySaving = month1 + month2;
+  let result = totallySaving;
+  console.log("Sabrian ha ahorrado en total " + result + " euros.");
+}
+saving(120, 85);
+
+//veintiseis
+function candyMaca(candy, distribution) {
+  const distributionCandyMaca = candy % distribution;
+  let result = distributionCandyMaca;
+  console.log("Sobrará " + result + " caramelo.");
+}
+candyMaca(73, 6);
+//veintisiete
+
+function record(morning, afternoon) {
+  const recordAbby = morning + afternoon;
+  let result = recordAbby;
+  console.log("En total registro un total de " + result + " casas.");
+}
+record(17, 8);
+
+//veintiocho
+function photosCuantity(photos, albums) {
+  const photosTotally = photos % albums;
+  let result = photosTotally;
+  console.log("Tendrá que dejar " + result + " fuera.");
+}
+photosCuantity(95, 12);
+
+//veintinueve
+function calculateDiscount(money, discount) {
+  const totallyDiscount = money - discount;
+  let result = totallyDiscount;
+  console.log("El producto tiene un precio final de " + result + " euros.");
+}
+calculateDiscount(120, 15);
+
+//treinta
+
+function flour(grams, portions) {
+  const portionsFlour = grams % portions;
+  let result = portionsFlour;
+  console.log("Sobrará " + result + " gramos de harina.");
+}
+flour(500, 24);
